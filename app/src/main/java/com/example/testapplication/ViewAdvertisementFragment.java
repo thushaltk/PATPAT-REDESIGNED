de5c26599684f32540905a9e29b36c5ddab6a642
Eishan-Dinuka-IT18118896 <eishan2dinuka@gmail.com>
2021-10-09 21:03:23 +0530

    Completed customer inquiry

--- app/src/main/java/com/example/testapplication/ViewAdvertisementFragment.java
+++ app/src/main/java/com/example/testapplication/ViewAdvertisementFragment.java
@@ -2,14 +2,19 @@ package com.example.testapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ViewAdvertisementFragment extends Fragment {
 
+    ImageView viewCustInquiry;
 
     public ViewAdvertisementFragment() {
         // Required empty public constructor
@@ -20,5 +25,22 @@ public class ViewAdvertisementFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_advertisement, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewCustInquiry = (ImageView) view.findViewById(R.id.imageView_cust);
+
+        viewCustInquiry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Fragment fragment = new CustomerInquiryFragment();
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .commit();
+            }
+        });
     }
 }
